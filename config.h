9a0ead84133c2b07a77065153d9f63a31c44d265
Paul Jewell <paul@teulu.org>
2021-03-04 10:29:44 +0000

    Changed delimiter, and moved sb-music entry
    For some reason, the sb-music entry is no longer showing. This is
    intended to help identify why.

--- config.h
+++ config.h
@@ -4,7 +4,6 @@ static const Block blocks[] = {
   /* {"⌨", "sb-kbselect", 0, 30}, */
   /*	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9}, */
   /* {"",	"sb-tasks",	10,	26},*/
-  {"",	"sb-music",	0,	11},
   /*{"",	"sb-pacpackages",	0,	8}, */
   /*{"",	"sb-news",		0,	6}, */
   /* {"",	"sb-crypto",	0,	13}, */
@@ -20,6 +19,7 @@ static const Block blocks[] = {
   /* {"",	"sb-moonphase",	18000,	17}, */
   /*	{"",	"sb-forecast",	18000,	5}, */
   /*{"",	"sb-mailbox",	180,	12},   */
+  {"",	"sb-music",	0,	11},
   {"",	"sb-nettraf",	1,	16},
   {"",	"sb-volume",	0,	10},
   {"",	"sb-battery",	5,	3},
@@ -29,7 +29,7 @@ static const Block blocks[] = {
 };
 
 //Sets delimiter between status commands. NULL character ('\0') means no delimiter.
-static char *delim = " ";
+static char *delim = " | ";
 
 // Have dwmblocks automatically recompile and run when you edit this file in
 // vim with the following line in your vimrc/init.vim:
